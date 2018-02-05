c40cd7419bf521412556fd480b8aed43e67c113f
Caleb Smith-Salzberg <csmithsalzberg@stuy.edu>
2018-02-05 13:17:46 -0500

    doesnt work on clear

--- clickdraw.js
+++ clickdraw.js
@@ -7,9 +7,6 @@ var c = document.getElementById("slate");
 //slate is the canvas
 var ctx = c.getContext("2d");
 //create a 2d object that controls the stuff
-ctx.fillStyle = "#ff0000";
-//red
-
 
 var toggle = document.getElementById("t");
 var clear = document.getElementById("c");
@@ -18,7 +15,7 @@ var clear = document.getElementById("c");
 var type = "circle"
 var mode = function(e){
     if (type == "circle"){
-	type = "rectangle";
+	type = "circle" //"rectangle"; //remove square stuff for now
     } else {
 	type =  "circle";
     }
@@ -26,18 +23,32 @@ var mode = function(e){
 }
 toggle.addEventListener("click", mode);
 
+var count = 0;
+
 var draw = function(e){
     console.log(e.offsetX);
     console.log(e.offsetY);
-    ctx.beginPath();
-    ctx.strokeStyle="red";
-    if (type =="circle"){
-	ctx.arc(e.offsetX, e.offsetY, 10, 0, 2*Math.PI);
+    
+    if (count = 0){
+	ctx.beginPath();
+	ctx.moveTo(e.offsetX,e.offsetY);
+	count += 1;
+    }
+    else{
+	ctx.lineTo(e.offsetX,e.offsetY);
 	ctx.stroke();
-	ctx.fill();
-    } else {
-	ctx.fillRect( e.offsetX, e.offsetY, 20, 20 );
-	//fill the rectangle with coordinates and lengths respectively
+	ctx.beginPath();
+	ctx.strokeStyle="red";
+	ctx.fillStyle="red";
+	if (type =="circle"){
+	    ctx.arc(e.offsetX, e.offsetY, 10, 0, 2*Math.PI);
+	    ctx.stroke();
+	    ctx.fill();
+	    ctx.moveTo(e.offsetX,e.offsetY);
+	} else {
+	    ctx.fillRect( e.offsetX, e.offsetY, 20, 20 );
+	    //fill the rectangle with coordinates and lengths respectively
+	}
     }
 }
 c.addEventListener("click", draw);
@@ -46,5 +57,6 @@ var clearboard = function(e){
     console.log(e);
     ctx.fillStyle="white";
     ctx.fillRect(0,0,600,600);
+    count=0;
 }
 clear.addEventListener("click", clearboard);
